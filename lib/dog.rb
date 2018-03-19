6c8f4b141e0ad7d403f72980cc7044f8535762e5
Jonathan Dust <d24dust@hotmail.com>
2018-03-19 17:48:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 class Dog
-  @this_dogs_name = ""
-  attr_accessor :name
-  def inintiate
-    self.name = @this_dogs_name
+
+  def name=(name)
+    @this_dogs_name = name
   end
+
+  def name
+    @this_dogs_name
+  end
+  
 end
